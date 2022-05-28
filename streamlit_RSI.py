8569488dfdf2ec9a96c0fce239c756fc3762397a
jiahongc <Jiahong1996@gmail.com>
2022-05-28 08:01:44 -0400

    Modified for app to run

--- streamlit_RSI.py
+++ streamlit_RSI.py
@@ -3,68 +3,71 @@ import altair as alt
 import pandas as pd
 import pandas_ta as ta
 import numpy as np
-from IPython.display import display_html
-from itertools import chain,cycle
+# from itertools import chain,cycle
 import yfinance as yf
 
-col1,col2 = st.columns(2)
-ticker="SPY"
-time="ytd"
-
+st.set_page_config(layout="wide")
+st.markdown("<h1 style='text-align: center; color: black;'><em>RSI</em></h1>   \n", unsafe_allow_html=True)
+st.markdown("   \n")
+st.markdown("   \n")
+st.markdown("   \n")
+st.markdown("   \n")
+col1,col_placeholder,col2 = st.columns([3,1,3])
 with col1:
     ticker = st.text_input('Enter Ticker: ')
+with col2:   
+    time = st.selectbox("Select Time", ("1mo","3mo","6mo","1y","2y","5y","10y","ytd","max"))
 
-with col2:
-    time = st.selectbox("Select Time", ("1d","5d","1mo","3mo","6mo","1y","2y","5y","10y","ytd","max"))
 
 
-df = yf.Ticker(ticker).history(period=time)
-df=df.reset_index()
-df['Date']=pd.to_datetime(df['Date'])
-df=df[['Date','Open','High','Low','Close']]
-df['Close']=df['Close']
-df['RSI']=ta.rsi(df['Close'], timeperiod=14)
+if ticker and time:
+    df = yf.Ticker(ticker).history(period=time)
+    df=df.reset_index()
+    df['Date']=pd.to_datetime(df['Date'])
+    df=df[['Date','Open','High','Low','Close']]
+    # df['Close']=np.round(df['Close'],2)
+    df['RSI']=ta.rsi(df['Close'], timeperiod=14)
 
-df.drop(range(0,14),inplace=True)
-df.reset_index(drop=True,inplace=True)
-df['Overbought']=70
-df['Oversold']=30
+    # df=df.drop(range(0,14))
+    df.dropna(inplace=True)
+    df=df.reset_index(drop=True)
+    df['Overbought']=70
+    df['Oversold']=30
 
-Stock = alt.Chart(df).mark_line(point=alt.OverlayMarkDef(color="grey",size=15)).encode(
-    x='Date:T',
-    y='Close:Q',
-    tooltip=[alt.Tooltip('date:T', format='%m/%d'),
-             alt.Tooltip('Close:Q', format=',.2f')]
-).properties(
-    title='Stock Price',
-    width=800,
-    height=300
-)
-RSI = alt.Chart(df).mark_line().encode(
-    x='Date:T',
-    y='RSI:Q',
-    tooltip=[alt.Tooltip('date:T', format='%m/%d'),
-             alt.Tooltip('Close:Q', format=',.2f'),
-             alt.Tooltip('RSI:Q', format=',.2f')]
-).properties(
-    title='RSI',
-    width=800,
-    height=300)
+    Stock = alt.Chart(df).mark_line(point=alt.OverlayMarkDef(color="grey",size=15)).encode(
+        x='Date:T',
+        y='Close:Q',
+        tooltip=[alt.Tooltip('date:T', format='%m/%d'),
+                alt.Tooltip('Close:Q', format=',.2f')]
+    ).properties(
+        title='Stock Price',
+        width=800,
+        height=300
+    ).interactive()
+    RSI = alt.Chart(df).mark_line().encode(
+        x='Date:T',
+        y='RSI:Q',
+        tooltip=[alt.Tooltip('date:T', format='%m/%d'),
+                alt.Tooltip('Close:Q', format=',.2f'),
+                alt.Tooltip('RSI:Q', format=',.2f')]
+    ).properties(
+        title='RSI',
+        width=800,
+        height=300).interactive()
 
-Oversold = alt.Chart(df).mark_line(color= 'red').encode(
-    x='Date:T',
-    y='Oversold:Q'
-)
+    Oversold = alt.Chart(df).mark_line(color= 'red').encode(
+        x='Date:T',
+        y='Oversold:Q'
+    )
 
-Overbought = alt.Chart(df).mark_line(color= 'green').encode(
-    x='Date:T',
-    y='Overbought:Q'
-)
+    Overbought = alt.Chart(df).mark_line(color= 'green').encode(
+        x='Date:T',
+        y='Overbought:Q'
+    )
 
-RSI_Chart=RSI+Oversold+Overbought
+    RSI_Chart=RSI+Oversold+Overbought
 
-with col1:
+
     st.altair_chart(Stock)
 
-with col2: 
     st.altair_chart(RSI_Chart)
